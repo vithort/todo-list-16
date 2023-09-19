a703f6cbab2408ed32883e0eda7fde873da0a16d
Vithor Carvalho <vithort@gmail.com>
2023-09-19 16:24:10 +0100

    72. implementando card de todo - parte 1

---
+++ src/app/components/todo-card/todo-card.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, computed, inject } from '@angular/core';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { TodoSignalsService } from 'src/app/services/todo-signals.service';
+import { TodoKeyLocalStorage } from 'src/app/models/enum/TodoKeyLocalStorage';
+import { Todo } from 'src/app/models/model/todo.model';
+
+@Component({
+  selector: 'app-todo-card',
+  standalone: true,
+  imports: [
+    NgFor,
+    NgIf,
+    NgTemplateOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+  ],
+  templateUrl: './todo-card.component.html',
+  styleUrls: [],
+})
+export class TodoCardComponent implements OnInit {
+  private todosSignalsService = inject(TodoSignalsService);
+  private todosSignal = this.todosSignalsService.todosState;
+
+  todosList = computed(() => this.todosSignal());
+
+  ngOnInit(): void {
+    this.getTodosInLocalStorage();
+  }
+
+  private getTodosInLocalStorage(): void {
+    const todosDatas = localStorage.getItem(
+      TodoKeyLocalStorage.TODO_LIST
+    ) as string;
+
+    todosDatas && this.todosSignal.set(JSON.parse(todosDatas));
+  }
+
+  private saveTodosInLocalStorage(): void {
+    this.todosSignalsService.saveTodosLocalStorage();
+  }
+
+  handleDoneTodo(todoId: number): void {
+    if (todoId) {
+      this.todosSignal.mutate((todos) => {
+        const todoSelected = todos.find((todo) => todo?.id === todoId) as Todo;
+        todoSelected && (todoSelected.done = true);
+      });
+    }
+  }
+
+  handleDeleteTodo(todo: Todo): void {
+    if (todo) {
+      const index = this.todosList().indexOf(todo);
+
+      if (index !== -1) {
+        this.todosSignal.mutate((todos) => todos.splice(index, 1));
+        this.saveTodosInLocalStorage();
+      }
+    }
+  }
+}
