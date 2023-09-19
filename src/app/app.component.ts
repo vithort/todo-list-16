d493fcd4be868b3f4f6c3e699a33151eb529b6d7
Vithor Carvalho <vithort@gmail.com>
2023-09-19 17:15:31 +0100

    73. implementando card de todo - parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
