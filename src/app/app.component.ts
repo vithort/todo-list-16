69407c043aecd44ea6dfa134476e0c7f62c1fa82
Vithor Carvalho <vithort@gmail.com>
2023-10-29 12:07:36 +0000

    Aprendendo utilizar operador Map e From

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { SchoolData, SchoolService } from './services/school.service';
-import { Observable, zip } from 'rxjs';
+import { Observable, zip, of, map, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +19,14 @@ export class AppComponent implements OnInit {
   students: Array<SchoolData> = [];
   tachers: Array<SchoolData> = [];
 
+  private ages = of(20, 30, 40, 50, 60, 70);
+
+  private peopleData = from([
+    { name: 'Vithor Carvalho', age: 20, profession: 'FE Developer' },
+    { name: 'Cesar', age: 25, profession: 'UX' },
+    { name: 'Caimi', age: 30, profession: 'BE Developer' },
+  ]);
+
   private zipSchoolResponse$ = zip(
     this.getStudentsData(),
     this.getTeachersData()
@@ -27,14 +35,28 @@ export class AppComponent implements OnInit {
   constructor(private schoolService: SchoolService) {}
 
   ngOnInit(): void {
-    this.getSchoolData();
+    // this.getSchoolData();
+    // this.getMultipliedAges();
+    this.getPeopleProfession();
+  }
+
+  getMultipliedAges(): void {
+    this.ages.pipe(map((age) => age * 2)).subscribe({
+      next: (response) => console.log('IDADE MULTIPLICADA:', response),
+    });
+  }
+
+  getPeopleProfession(): void {
+    this.peopleData.pipe(map((people) => people.profession)).subscribe({
+      next: (response) => console.log('PROFISSAO:', response),
+    });
   }
 
   getSchoolData(): void {
     this.zipSchoolResponse$.subscribe({
       next: (response) => {
-        console.log('STUDENTS', response[0]);
-        console.log('TEACHERS', response[1]);
+        console.log('STUDENTS:', response[0]);
+        console.log('TEACHERS:', response[1]);
       },
     });
   }
